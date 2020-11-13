2746d8e18a5b8a9d1b978147a68369223c881497
ANeshkova <anitowurth@abv.bg>
2020-11-13 11:52:29 +0200

    Added solution

---
+++ 06.2 Subqueries and JOINs - Exercise/16. Countries without any Mountains.sql
@@ -0,0 +1,8 @@
+USE `geography`;
+
+SELECT COUNT(c.`country_code`) - COUNT(`mountain_id`) AS `country_code` 
+FROM `mountains` AS m
+RIGHT JOIN `mountains_countries` AS mc
+ON m.`id` = mc.`mountain_id`
+RIGHT JOIN `countries` AS c
+ON mc.`country_code` = c.`country_code`;

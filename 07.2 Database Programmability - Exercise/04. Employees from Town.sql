0015c9f440478f7b1452503280224a1b922608b0
ANeshkova <anitowurth@abv.bg>
2020-11-17 10:21:35 +0200

    Added solution

---
+++ 07.2 Database Programmability - Exercise/04. Employees from Town.sql
@@ -0,0 +1,18 @@
+USE `soft_uni`;
+
+-- FOR JUDGE TESTING SYSTEM(without delimiter):
+
+-- DELIMITER $$
+CREATE PROCEDURE `usp_get_employees_from_town` (town_name VARCHAR(40))
+BEGIN
+	SELECT e.`first_name`, e.`last_name`
+	FROM `employees` AS e
+	JOIN `addresses` AS a
+	ON a.`address_id` = e.`address_id`
+	JOIN `towns` AS t
+	ON t.`town_id` = a.`town_id`
+	WHERE t.`name` = town_name
+	ORDER BY e.`first_name`, e.`last_name`, e.`employee_id`;
+END; -- END$$
+
+-- DELIMITER ;

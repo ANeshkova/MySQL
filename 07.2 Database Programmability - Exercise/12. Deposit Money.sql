dc7b9c30962a91d5f9fedd675e318f25a8896b71
ANeshkova <anitowurth@abv.bg>
2020-11-18 14:32:53 +0200

    Added solution

---
+++ 07.2 Database Programmability - Exercise/12. Deposit Money.sql
@@ -0,0 +1,19 @@
+USE `soft_uni`; -- After run 'bank_db.sql'
+
+-- FOR JUDGE TESTING SYSTEM(without delimiter):
+
+-- DELIMITER $$
+CREATE PROCEDURE `usp_deposit_money`(account_id INT, money_amount DECIMAL(19,4))
+BEGIN
+	START TRANSACTION;
+    IF(SELECT COUNT(*) FROM accounts WHERE id = account_id) = 0
+		OR (money_amount <= 0)
+		THEN ROLLBACK;
+	ELSE
+		UPDATE accounts
+        SET balance = balance + money_amount
+        WHERE id = account_id;
+	END IF;
+END; -- END$$
+
+-- DELIMITER ;

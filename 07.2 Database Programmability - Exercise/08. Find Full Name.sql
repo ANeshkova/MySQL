40e69fd87b1d2e7022ce9681dbab577c9c3a4c19
ANeshkova <anitowurth@abv.bg>
2020-11-18 14:27:54 +0200

    Added solution

---
+++ 07.2 Database Programmability - Exercise/08. Find Full Name.sql
@@ -0,0 +1,13 @@
+USE `soft_uni`; -- After run 'bank_db.sql'
+
+-- FOR JUDGE TESTING SYSTEM(without delimiter):
+
+-- DELIMITER $$
+CREATE PROCEDURE `usp_get_holders_full_name` ()
+BEGIN
+	SELECT CONCAT_WS(' ', ah.`first_name`, ah.`last_name`) AS `full_name` 
+    FROM `account_holders` AS ah
+    ORDER BY `full_name`, `id`;
+END; -- END$$
+
+-- DELIMITER ;

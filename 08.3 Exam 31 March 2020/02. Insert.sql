5c60baf1be40d1b63f7c283f9a78318db866dc9f
ANeshkova <anitowurth@abv.bg>
2020-11-25 12:38:19 +0200

    Added solution

---
+++ 08.3 Exam 31 March 2020/02. Insert.sql
@@ -0,0 +1,8 @@
+USE `instd`;
+
+INSERT INTO `addresses` (`address`, `town`, `country`, `user_id`)
+(
+	SELECT u.`username`, u.`password`, u.`ip`, u.`age`
+	FROM `users` AS u
+	WHERE u.`gender` = 'm'
+);

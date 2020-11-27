a5e13554194455ad2811a17f6ad9caab4205bbbb
ANeshkova <anitowurth@abv.bg>
2020-11-27 13:53:26 +0200

    Added solution

---
+++ 08.3 Exam 31 March 2020/06. Extract 5 most commented photos.sql
@@ -0,0 +1,10 @@
+USE `instd`;
+
+SELECT p.`id`, p.`date` AS 'date_and_time',
+	p.`description`, COUNT(c.`id`) AS 'commentsCount'
+FROM `photos` AS p
+JOIN `comments` AS c
+ON p.`id` = c.`photo_id`
+GROUP BY p.`id`
+ORDER BY `commentsCount` DESC, p.`id`
+LIMIT 5;

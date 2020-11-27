2098c255075481404ca561445d36a22cc2bd8962
ANeshkova <anitowurth@abv.bg>
2020-11-27 13:55:43 +0200

    Added solution

---
+++ 08.3 Exam 31 March 2020/07. Lucky users.sql
@@ -0,0 +1,8 @@
+USE `instd`;
+
+SELECT CONCAT_WS(' ', u.`id`, u.`username`) AS 'id_username', u.`email`
+FROM `users` AS u
+JOIN `users_photos` AS up
+ON u.`id` = up.`user_id`
+WHERE u.`id` = up.`photo_id`
+ORDER BY u.`id`;

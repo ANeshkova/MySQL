d09b5d52da9920f1b561e9fc850eba56368c0667
ANeshkova <anitowurth@abv.bg>
2020-11-27 13:57:00 +0200

    Added solution

---
+++ 08.3 Exam 31 March 2020/10. Get user’s photos count.sql
@@ -0,0 +1,19 @@
+USE `instd`;
+
+-- FOR JUDGE TESTING SYSTEM(without delimiter):
+
+-- DELIMITER $$
+CREATE FUNCTION `udf_users_photos_count` (username VARCHAR(30))
+RETURNS INTEGER
+DETERMINISTIC
+BEGIN
+	RETURN (
+		SELECT COUNT(up.`photo_id`)
+		FROM `users` AS u
+		JOIN `users_photos` AS up
+		ON u.`id` = up.`user_id`
+		WHERE u.`username` = username
+    );
+END; -- END$$
+
+-- DELIMITER ;

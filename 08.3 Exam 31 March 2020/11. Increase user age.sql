224d0c14b82e336a80e96ea3646288f05cb8c281
ANeshkova <anitowurth@abv.bg>
2020-11-28 11:58:48 +0200

    Added solution

---
+++ 08.3 Exam 31 March 2020/11. Increase user age.sql
@@ -0,0 +1,15 @@
+USE `instd`;
+
+-- FOR JUDGE TESTING SYSTEM(without delimiter):
+
+-- DELIMITER $$
+CREATE PROCEDURE `udp_modify_user` (address VARCHAR(30), town VARCHAR(30))
+BEGIN
+	UPDATE `users` AS u
+	JOIN `addresses` AS a
+	ON u.`id` = a.`user_id`
+	SET u.`age` = u.`age` + 10
+    WHERE a.`address` = address AND a.`town` = town;
+END; -- END$$
+
+-- DELIMITER ;
